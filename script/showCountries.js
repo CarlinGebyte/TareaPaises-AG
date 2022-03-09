7056e6bf8d5bd9b08c09e455f951eef0893c3662
CarlinGebyte <91474067+CarlinGebyte@users.noreply.github.com>
2022-03-08 21:05:05 -0500

    Update showCountries adding card content

--- script/showCountries.js
+++ script/showCountries.js
@@ -1,9 +1,19 @@
 export const showCountries = async (arrayCountries, cards) => {
   const countries = await arrayCountries;
   countries.forEach((country) => {
-    const { id, name, region, population, capital } = country;
+    const { id, flags, name, region, population, capital } = country;
     cards.innerHTML += `
-      
+      <div class="card">
+        <div class="card-body" >
+        <img src=${flags.png} class="card-img-top" alt="...">
+          <h5 class="card-title">${name}</h5>
+          <p>País: ${name}</p>
+          <p>Región: ${region}</p>
+          <p>Población: ${population}</p>
+          <p>Capital: ${capital}</p>
+          <a href="#" id=${id} class="btn btn-outline-dark " style="background-color: #db7093;">Detail</a>
+        </div>
+      </div>
     `;
   });
 };
